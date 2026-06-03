7f127412369fe3f52745a5e8ede5d83e8edb0d08
prince <princesingh363626@gmail.com>
2026-06-03 22:38:36 +0530

    week 1 day 3 - containerwithmostwater.cpp

---
+++ Week_1/Day_3/container_with_most_water.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left=0,right=height.size()-1;
+        int area = 0;
+        while(left<right){
+            area = max(area,((right-left)*min(height[left],height[right])));
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return area;
+
+        
+    }
+};
