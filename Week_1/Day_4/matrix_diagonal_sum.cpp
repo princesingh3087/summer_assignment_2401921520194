5f4dc3fb73d622fc19b3333dfb9b9fee1eb22aef
prince <princesingh363626@gmail.com>
2026-06-04 22:48:26 +0530

    week 1 day 4 - matrixdiagonalsum.cpp

---
+++ Week_1/Day_4/matrix_diagonal_sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum =0;
+        for(int i =0;i<mat.size();i++){
+            
+             sum = sum + mat[i][i];
+            
+        if(i!=n-1-i){
+                sum = sum+mat[i][n-1-i];
+            }
+        }
+        return sum;
+        
+    }
+};
