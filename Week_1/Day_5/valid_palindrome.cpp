3bbfa45bf3cf3f5740b6623c580c8beb43d6bd2f
prince <princesingh363626@gmail.com>
2026-06-05 20:07:53 +0530

    week 1 day 5 - validpalindrome.cpp

---
+++ Week_1/Day_5/valid_palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+ bool isalpha(char ch){
+            if((ch>='0'&&ch<='9')||(tolower(ch)>='a'&&tolower(ch)<='z')){
+                return true;
+            }
+            return false;
+        }
+    bool isPalindrome(string s) {
+      
+        int start = 0;
+        int end = s.size()-1;
+        while(start<end){
+            if(isalpha(s[start])==false){
+                start++;
+                continue;
+            }
+            if(isalpha(s[end])==false){
+                end --;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+};
