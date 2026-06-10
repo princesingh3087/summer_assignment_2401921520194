612a0cf9b1d694aadca1742e838b274ad328895c
prince <princesingh363626@gmail.com>
2026-06-10 22:37:47 +0530

    week 2 day 3 - issubsequence.cpp

---
+++ Week_2/Day_3/issubsequence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0;
+        
+        int count =0;
+        
+        for(int j=0;j<t.size();j++){
+            if(s[i]==t[j]){
+                i++;
+                count++;
+            }
+            // else{
+            //     j++;
+            // }
+        }
+        if(count == s.size()){
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+};
