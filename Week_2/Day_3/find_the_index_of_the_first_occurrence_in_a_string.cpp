b95b6e173677c1cbe16b86c67d73e5fc027c77e7
prince <princesingh363626@gmail.com>
2026-06-10 23:31:29 +0530

    week 2 day 3 - findtheindexofthefirstoccurrenceinastring.cpp

---
+++ Week_2/Day_3/find_the_index_of_the_first_occurrence_in_a_string.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i=0 ; i<haystack.size();i++){
+            for(int j=0 ; j<needle.size();j++){
+                if(haystack[i+j]!=needle[j]){
+                    break;
+                }
+                else if(j==needle.size()-1){
+                    return i;
+                }
+            }
+        }
+        return -1;
+
+        
+    }
+};
